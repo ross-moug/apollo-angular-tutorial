c8dd1644db661e27d6a706ea4bc9c61662090085
Ross Moug <ross.moug@solarwinds.com>
2020-02-14 16:18:19 +0000

    🚀 Add component to query GraphQL data

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,14 @@ import { NgModule } from "@angular/core";
 
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
+import { ExchangeRatesComponent } from "./exchange-rates/exchange-rates.component";
 import { GraphQLModule } from "./graphql.module";
 import { HttpClientModule } from "@angular/common/http";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ExchangeRatesComponent,
   ],
   imports: [
     BrowserModule,
---
+++ src/app/exchange-rates/exchange-rates.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit } from '@angular/core';
+import { Apollo } from "apollo-angular";
+import gql from 'graphql-tag';
+
+interface ExchangeRates {
+  rates: any[];
+}
+
+@Component({
+  selector: 'app-exchange-rates',
+  templateUrl: './exchange-rates.component.html',
+})
+export class ExchangeRatesComponent implements OnInit {
+  rates: any[];
+  loading = true;
+  error: any;
+
+  constructor(private apollo: Apollo) {}
+
+  ngOnInit(): void {
+    this.apollo
+      .watchQuery<ExchangeRates>({
+        query: gql`
+          {
+            rates(currency: "USD") {
+              currency
+              rate
+            }
+          }
+        `,
+      })
+      .valueChanges.subscribe(result => {
+      this.rates = result.data && result.data.rates;
+      this.loading = result.loading;
+      this.error = result.errors;
+    });
+  }
+}

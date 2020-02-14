8f1baaae5768e3d8794ffa97ceacc5649c6c5147
Ross Moug <ross.moug@solarwinds.com>
2020-02-14 16:10:11 +0000

    Base commit 🎉

---
+++ src/app/graphql.module.ts
@@ -0,0 +1,31 @@
+import { NgModule } from "@angular/core";
+import { ApolloModule, APOLLO_OPTIONS } from "apollo-angular";
+import { HttpLinkModule, HttpLink } from "apollo-angular-link-http";
+import { InMemoryCache, NormalizedCacheObject } from "apollo-cache-inmemory";
+import { ApolloClientOptions } from "apollo-client";
+
+const uri: string = "https://o5x5jzoo7z.sse.codesandbox.io/graphql"; // <-- add the URL of the GraphQL server here
+export function createApollo(httpLink: HttpLink): ApolloClientOptions<NormalizedCacheObject> {
+  return {
+    link: httpLink.create({
+      uri: uri
+    }),
+    cache: new InMemoryCache(),
+  };
+}
+
+@NgModule({
+  exports: [
+    ApolloModule,
+    HttpLinkModule
+  ],
+  providers: [
+    {
+      provide: APOLLO_OPTIONS,
+      useFactory: createApollo,
+      deps: [HttpLink],
+    },
+  ],
+})
+export class GraphQLModule {
+}

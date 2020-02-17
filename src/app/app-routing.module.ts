d8273c39c62401ee1ab00ca2a27b1d95e6f6baf9
Ross Moug <ross.moug@solarwinds.com>
2020-02-17 12:26:16 +0000

    Add routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
+import { ExchangeRatesComponent } from "./exchange-rates/exchange-rates.component";
+import { ProfileComponent } from "./profile/profile.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "basic",
+    component: ExchangeRatesComponent
+  },
+  {
+    path: "queries",
+    component: ProfileComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

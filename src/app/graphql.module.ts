ee8a8034108eec1e00d57a80f98ed7721eb4e4d2
Ross Moug <ross.moug@solarwinds.com>
2020-02-18 14:15:54 +0000

    ApolloLink

--- src/app/graphql.module.ts
+++ src/app/graphql.module.ts
@@ -1,15 +1,50 @@
 import { NgModule } from "@angular/core";
 import { ApolloModule, APOLLO_OPTIONS } from "apollo-angular";
-import { HttpLinkModule, HttpLink } from "apollo-angular-link-http";
+import { HttpLinkModule, HttpLink, HttpLinkHandler } from "apollo-angular-link-http";
 import { InMemoryCache, NormalizedCacheObject } from "apollo-cache-inmemory";
 import { ApolloClientOptions } from "apollo-client";
+import { ApolloLink, from } from "apollo-link";
+import { onError } from "apollo-link-error";
+import { ServerError, ServerParseError } from "apollo-link-http-common";
 
 const uri = "https://o5x5jzoo7z.sse.codesandbox.io/graphql"; // <-- add the URL of the GraphQL server here
 export function createApollo(httpLink: HttpLink): ApolloClientOptions<NormalizedCacheObject> {
+  const http: HttpLinkHandler = httpLink.create({ uri });
+
+  const authMiddleware: ApolloLink = new ApolloLink((operation, forward) => {
+    operation.setContext(({ headers }) => {
+      if (headers) {
+        return {
+          headers: headers.append("Authorization", localStorage.getItem("token") || null),
+        };
+      }
+    });
+
+    return forward(operation);
+  });
+
+  const otherMiddleware: ApolloLink = new ApolloLink((operation, forward) => {
+    operation.setContext(({ headers }) => {
+      if (headers) {
+        return {
+          headers: headers.append("recent-activity", localStorage.getItem("lastOnlineTime") || null)
+        };
+      }
+    });
+
+    return forward(operation);
+  });
+
+  const logoutLink: ApolloLink = onError(({ networkError }) => {
+    if (Object.getOwnPropertyDescriptor(networkError, "statusCode")
+      && (networkError as ServerError | ServerParseError).statusCode === 401) {
+      console.log("User logged-out!");
+      // Handle log-out action
+    }
+  });
+
   return {
-    link: httpLink.create({
-      uri,
-    }),
+    link: logoutLink.concat(from([authMiddleware, otherMiddleware, http])),
     cache: new InMemoryCache(),
   };
 }
